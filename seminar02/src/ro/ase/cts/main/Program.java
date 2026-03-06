ebd65342c6212f36bf5ce348876f3662922f8aa3
elena <elenatanase320@gmail.com>
2026-03-06 09:21:24 +0200

    Seminar 3

--- seminar02/src/ro/ase/cts/main/Program.java
+++ seminar02/src/ro/ase/cts/main/Program.java
@@ -14,8 +14,11 @@ public class Program {
 		AplicantiLoader loader = new AngajatiLoader();
 		try {
 			listaAngajati = loader.load("seminar02/angajati.txt");
-			for(Aplicant angajat:listaAngajati)
+			for(Aplicant angajat:listaAngajati){
 				System.out.println(angajat.toString());
+				angajat.afisareFinantare();
+				angajat.afisareStatut();
+			}
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
